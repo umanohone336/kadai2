b838829a3269072f8bcfc2cdcf1465288082ca66
EC2 Default User <ec2-user@ip-172-31-9-149.ap-northeast-1.compute.internal>
2022-12-14 09:44:30 +0000

    [add] 詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  # get 'url' => 'コントローラー名#アクション名'1. ユーザーがRailsアプリケーションのURLにアクセスする
+  #2. そのURLに対応したコントローラとアクションを呼び出す
+  #3. そのアクションに対応したビューを表示する
+  get 'books/new' => 'books#new'
   get '/top' => 'homes#top'
-
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
+  get 'books/edit'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
